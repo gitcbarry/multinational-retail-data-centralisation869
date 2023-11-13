5d6bd95178cbec378965084b6cbba619b44d8abe
Chris Barry <cbarry@hep.ph.liv.ac.uk>
2023-11-13 20:19:33 +0000

    First pass of database_utils.py Reading the database and listing the tables available

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,56 @@
+import yaml 
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+
+import psycopg2
+
 class DatabaseConnector:
   '''
   Connect and upload data to the database
+
+  Methods:
+    -------
+    read_db_creds 
+      Read the YAML credentials file into a dictionary
   '''
+  def read_db_creds(self, yaml_file):
+    '''
+    Read the YAML credentials file and return a dictionary
+    '''
+    with open(yaml_file, 'r') as f:
+      yaml_creds = yaml.safe_load(f)
+    
+    return yaml_creds
+  
+  def init_db_engine(self,yaml_creds):
+    '''
+    Takes the credentials from the `read_db_creds` method and initalises and 
+    returns a sqlalchemy database engine
+    '''
+    DATABASE_TYPE = 'postgresql'
+    DBAPI = 'psycopg2'
+    db_string = f"{DATABASE_TYPE}+{DBAPI}://{yaml_creds['RDS_USER']}:{yaml_creds['RDS_PASSWORD']}@{yaml_creds['RDS_HOST']}:{yaml_creds['RDS_PORT']}/{yaml_creds['RDS_DATABASE']}"
+    print (db_string)
+    db_engine = create_engine(db_string)
+    return db_engine
+  
+  def list_db_tables(self, db_engine):
+    '''
+    Lists all the tables in the database to extract data from 
+    '''
+    db_table_list = []
+    inspector = inspect(db_engine)
+
+    for table_name in inspector.get_table_names():
+      for column in inspector.get_columns(table_name):
+        db_table_list.append(column['name'])
+        print("Column: %s" % column['name'])
+
+    print(db_table_list)
+    return db_table_list    
+
+if __name__ == '__main__':
+  d_b = DatabaseConnector()
+  yaml_creds = d_b.read_db_creds("db_creds.yaml")
+  engine = d_b.init_db_engine(yaml_creds)
+  d_b.list_db_tables(engine)
